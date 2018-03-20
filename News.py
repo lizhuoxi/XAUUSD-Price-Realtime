1fbcd0e82a47f48a8ffaac34e4c5094944b8f4e1
kwongtai <kwongtailau@gmail.com>
2018-03-21 00:16:45 +0800

    fix bug clear()

--- News.py
+++ News.py
@@ -1,10 +1,36 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
 import requests
 from datetime import *
 from time import sleep
 
+# history url
+history_url = 'https://kuaixun.cngold.org/getLastNews.html?versionType=new'
+
+# lastest url
+lastest_url = 'https://kuaixun.cngold.org/getLastNews.html?versionType=new&lastTime='
+
+# header info
+headers={'authority':'kuaixun.cngold.org',
+        'Connection': 'keep-alive',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36',
+        'Accept': '*/*',
+        'Referer': 'http://www.cngold.org/quote/',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+         }
+
 def NewsFilter(raw_data,count):
+    '''
+    get the main info from news
+    :param raw_data:
+    :param count:
+    :return:
+    '''
     news_list = []
-    news_list.clear()
+    # bug: Python2 does not have func clear(), in this I use del to fix.
+    # news_list.clear()
     last_time = 0
     for i in range(count):
         time_data = raw_data[i]['pubTime']
@@ -16,34 +42,35 @@ def NewsFilter(raw_data,count):
         news_title = raw_data[i]['title']
         news = news_datetime + ' ' + news_title
         news_list.append(news)
-    return news_list,last_time
+    ret = news_list,last_time
+    del news_list
+    return ret
 
 def GetHistoryNews():
-    res = requests.get('https://kuaixun.cngold.org/getLastNews.html?versionType=new',headers={'authority':'kuaixun.cngold.org',
-    'Connection': 'keep-alive',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36',
-    'Accept': '*/*',
-    'Referer': 'http://www.cngold.org/quote/',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'zh-CN,zh;q=0.9',})
-
+    '''
+    to get the history news from jintou
+    :return:
+    '''
+    res = requests.get(url=history_url, headers=headers)
     return NewsFilter(res.json()['data']['data'],res.json()['data']['count'])
 
 def GetLatestNews(timestamp):
-    res = requests.get('https://kuaixun.cngold.org/getLastNews.html?versionType=new&lastTime=' + str(timestamp),headers={'authority':'kuaixun.cngold.org',
-    'Connection': 'keep-alive',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36',
-    'Accept': '*/*',
-    'Referer': 'http://www.cngold.org/quote/',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'zh-CN,zh;q=0.9',})
-  
+    '''
+    to get the lastest new from jintou
+    :param timestamp:
+    :return:
+    '''
+    res = requests.get(url=lastest_url + str(timestamp),headers=headers)
     if(res.json()['data']['count'] > 0):
         return NewsFilter(res.json()['data']['data'],res.json()['data']['count'])
     else:
         return None,timestamp
 
-if __name__ == '__main__':
+def main():
+    '''
+    main func
+    :return:
+    '''
     news_list,ts = GetHistoryNews()
     if not news_list is None:
         news_list.reverse()
@@ -55,3 +82,6 @@ if __name__ == '__main__':
         if not news_list is None:
             for news in news_list:
                 print(news)
+
+if __name__ == '__main__':
+    main()
